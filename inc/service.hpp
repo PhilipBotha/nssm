18a3ba660f64b7b189c015435539c219f051fbb8
Philip <philip_smc@prbotha.net>
2024-02-18 23:07:36 +0200

    Creating Service_t replacement for nssm_service_t

--- inc/service.hpp
+++ inc/service.hpp
@@ -6,6 +6,8 @@
 #define NSSM_SERVICE_HPP
 #include "helper.hpp"
 #include "wexception.hpp"
+#include <cstdint>
+#include <filesystem>
 #include <windows.h>
 namespace nssm {
     //enum class Services { NSSM, ALL };
@@ -50,6 +52,93 @@ namespace nssm {
 
     private:
         SC_HANDLE mpHandle{nullptr};
+    };
+
+    class Service_t {
+        bool native;
+        std::wstring name{};
+        std::wstring displayname{};
+        std::wstring description{};
+        unsigned long startup;
+        std::wstring username{};
+        std::wstring password;
+        unsigned long type;
+        std::wstring image{};
+        std::wstring exe{};
+        std::wstring flags{};
+        std::wstring dir{};
+        std::wstring env{};
+        std::int64_t affinity;
+        std::wstring dependencies{};
+        std::wstring env_extra{};
+        unsigned long priority;
+        unsigned long no_console;
+        std::filesystem::path stdin_path{};
+        unsigned long stdin_sharing;
+        unsigned long stdin_disposition;
+        unsigned long stdin_flags;
+        std::filesystem::path stdout_path{};
+        unsigned long stdout_sharing;
+        unsigned long stdout_disposition;
+        unsigned long stdout_flags;
+        bool use_stdout_pipe;
+        HANDLE stdout_si;
+        HANDLE stdout_pipe;
+        HANDLE stdout_thread;
+        unsigned long stdout_tid;
+        std::filesystem::path stderr_path{};
+        unsigned long stderr_sharing;
+        unsigned long stderr_disposition;
+        unsigned long stderr_flags;
+        bool use_stderr_pipe;
+        HANDLE stderr_si;
+        HANDLE stderr_pipe;
+        HANDLE stderr_thread;
+        unsigned long stderr_tid;
+        bool hook_share_output_handles;
+        bool rotate_files;
+        bool timestamp_log;
+        bool stdout_copy_and_truncate;
+        bool stderr_copy_and_truncate;
+        unsigned long rotate_stdout_online;
+        unsigned long rotate_stderr_online;
+        unsigned long rotate_seconds;
+        unsigned long rotate_bytes_low;
+        unsigned long rotate_bytes_high;
+        unsigned long rotate_delay;
+        unsigned long default_exit_action;
+        unsigned long restart_delay;
+        unsigned long throttle_delay;
+        unsigned long stop_method;
+        unsigned long kill_console_delay;
+        unsigned long kill_window_delay;
+        unsigned long kill_threads_delay;
+        bool kill_process_tree;
+        SC_HANDLE handle;
+        SERVICE_STATUS status;
+        SERVICE_STATUS_HANDLE status_handle;
+        HANDLE process_handle;
+        unsigned long pid;
+        HANDLE wait_handle;
+        unsigned long exitcode;
+        bool stopping;
+        bool allow_restart;
+        unsigned long throttle;
+        CRITICAL_SECTION throttle_section;
+        bool throttle_section_initialised;
+        CRITICAL_SECTION hook_section;
+        bool hook_section_initialised;
+        CONDITION_VARIABLE throttle_condition;
+        HANDLE throttle_timer;
+        LARGE_INTEGER throttle_duetime;
+        FILETIME nssm_creation_time;
+        FILETIME creation_time;
+        FILETIME exit_time;
+        std::wstring initial_env{};
+        unsigned long last_control;
+        unsigned long start_requested_count;
+        unsigned long start_count;
+        unsigned long exit_count;
     };
 } // namespace nssm
 
